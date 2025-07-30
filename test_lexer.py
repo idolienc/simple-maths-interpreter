fb2c8cfa4a3cc984f27abc25c65ec49f990994eb
idolienc <gwardgaming@gmail.com>
2025-07-30 22:43:29 +0100

    Adding tests, not complete getting assistance from py disc

--- test_lexer.py
+++ test_lexer.py
@@ -1,4 +1,22 @@
 from lexer import Lexer
+import pytest
 
 def test_one():
     assert Lexer("2+2") == ["2", "+", "2"]
+
+def test_two():
+    assert Lexer("2.2+2") == ["2.2", "+", "2"]
+
+def test_three():
+    assert Lexer("2 + 2") == ["2", "+", "2"]
+
+def test_four():
+    assert Lexer("2-2*2/2") == ["2", "-", "2", "*", "2", "/", "2"]
+
+def test_bad_input():
+    with pytest.raises(TypeError):
+        assert Lexer("2.2.2+2")
+
+def test_multi_operators():
+    with pytest.raises(TypeError):
+        assert Lexer("2++2+2")
